c8cfa662c55b97bfa01f9b413ec3ee69634147ff
nganlamforwork <nganlamforwork@gmail.com>
2023-01-23 03:25:20 +0700

    fix conflict

--- src/modules/Home/Sections/Hero.tsx
+++ src/modules/Home/Sections/Hero.tsx
@@ -4,38 +4,6 @@ import { ArrowDownwardOutlined } from "@mui/icons-material";
 import { Button, Carousel, Typography } from "../../../components";
 
 const Hero = () => {
-<<<<<<< HEAD
-    return (
-        <Box sx={{ position: "relative", width: "100%", height: "90vh" }}>
-            <Overlay>
-                <Box className="hero-introduction">
-                    <Typography size="h1" color="white" weight="extraBold">
-                        THUỐC
-                    </Typography>
-                    <Typography size="h2" color="white" weight="extraBold">
-                        VÀ
-                    </Typography>
-                    <Typography size="h1" color="white" weight="extraBold">
-                        SỨC KHOẺ
-                    </Typography>
-                    <Typography size="p" color="white" weight="light">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
-                        ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                    </Typography>
-                    <Button
-                        bgColor="primary"
-                        sx={{ marginTop: "1.4rem" }}
-                        endIcon={<ArrowDownwardOutlined sx={{ fontSize: "1.2rem", paddingLeft: "0.2rem" }} />}>
-                        <Typography size="p" color="inherit" weight="medium">
-                            Đọc thêm
-                        </Typography>
-                    </Button>
-                </Box>
-            </Overlay>
-            <Box sx={{ position: "absolute", zIndex: -999, height: "inherit" }}>
-                <Carousel listData={dataList} />
-            </Box>
-=======
   return (
     <Box sx={{ position: "relative", width: "100%", height: "90vh" }}>
       <Overlay>
@@ -68,9 +36,13 @@ const Hero = () => {
               Đọc thêm
             </Typography>
           </Button>
->>>>>>> 2b0105034f352842fbcaf2ed4a6098cee735a1f4
         </Box>
-    );
+      </Overlay>
+      <Box sx={{ position: "absolute", zIndex: -999, height: "inherit" }}>
+        <Carousel listData={dataList} />
+      </Box>
+    </Box>
+  );
 };
 
 export default Hero;
@@ -78,44 +50,45 @@ export default Hero;
 // ------------------------------------OVERLAY------------------------------------
 
 const Overlay = styled(Box)(({ theme }) => ({
+  position: "absolute",
+  top: 0,
+  right: 0,
+  background:
+    "linear-gradient(70deg, transparent 50%, rgba(113, 191, 218, 0.6) 50%)",
+  width: "100%",
+  height: "inherit",
+  textAlign: "right",
+  "& .hero-introduction": {
     position: "absolute",
-    top: 0,
-    right: 0,
-    background: "linear-gradient(70deg, transparent 50%, rgba(113, 191, 218, 0.6) 50%)",
-    width: "100%",
-    height: "inherit",
-    textAlign: "right",
-    "& .hero-introduction": {
-        position: "absolute",
-        top: "5rem",
-        right: "3rem",
-        width: "40%",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "end",
-        justifyContent: "center"
-    }
+    top: "5rem",
+    right: "3rem",
+    width: "40%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "end",
+    justifyContent: "center",
+  },
 }));
 
 // ------------------------------------CAROUSEL------------------------------------
 
 const imgLinks = [
-    "https://images.unsplash.com/photo-1666214280259-ab57309450b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-    "https://images.unsplash.com/photo-1532938911079-1b06ac7ceec7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80"
+  "https://images.unsplash.com/photo-1666214280259-ab57309450b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  "https://images.unsplash.com/photo-1532938911079-1b06ac7ceec7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80",
 ];
 
 const CarouselItem = styled(Box)(({ theme }) => ({
-    height: "100%",
-    display: "flex",
-    alignItems: "stretch",
-    backgroundColor: "red",
-    "& img": {
-        objectFit: "cover !important"
-    }
+  height: "100%",
+  display: "flex",
+  alignItems: "stretch",
+  backgroundColor: "red",
+  "& img": {
+    objectFit: "cover !important",
+  },
 }));
 
 const dataList = imgLinks.map((link) => (
-    <CarouselItem>
-        <img alt="" src={link} />
-    </CarouselItem>
+  <CarouselItem>
+    <img alt="" src={link} />
+  </CarouselItem>
 ));
--- src/modules/Home/Sections/components/SimpleCard.tsx
+++ src/modules/Home/Sections/components/SimpleCard.tsx
@@ -3,74 +3,20 @@ import React from "react";
 import { Typography } from "../../../../components";
 
 const Root = styled(Grid)(({ theme }) => ({
-<<<<<<< HEAD
-    cursor: "pointer",
-    transition: "all .1s ease-in-out",
-    "& .media-container": {
-        position: "relative",
-        overflow: "hidden",
-        "& .media-image": {
-            width: "100%",
-            height: "100%"
-        },
-        "& .media-overlay": {
-            position: "absolute",
-            width: "100%",
-            height: "100%",
-            backgroundColor: "rgba(7, 27, 33, 0.3)"
-        }
-    },
-    "&:hover": {
-        color: "var(--palette-02)",
-        "& .media-overlay": {
-            backgroundColor: "rgba(7, 27, 33, 0)"
-        }
-    }
-}));
-
-interface SimpleCardProps {
-    title?: string;
-    content?: string;
-    img?: string;
-    imgRatio?: number;
-    contentRatio?: number;
-    overlay?: boolean;
-    sx?: object;
-}
-const SimpleCard = ({ title = "", content = "", img = "", imgRatio = 6, contentRatio = 6, overlay = true, sx = {}, ...props }: SimpleCardProps) => {
-    return (
-        <Root container sx={sx} {...props} direction="column" spacing={2}>
-            <Grid item xs={imgRatio} className="media-container">
-                {overlay === true ? <Box className="media-overlay"></Box> : <></>}
-                <CardMedia image={img} title="" className="media-image" />
-            </Grid>
-            <Grid item xs={contentRatio}>
-                <Typography size="h6" weight="bold">
-                    {title}
-                </Typography>
-                <Typography size="p">{content}</Typography>
-            </Grid>
-        </Root>
-    );
-=======
   cursor: "pointer",
   transition: "all .1s ease-in-out",
-  overflow: "hidden",
   "& .media-container": {
     position: "relative",
     overflow: "hidden",
-    borderRadius: "inherit",
     "& .media-image": {
       width: "100%",
       height: "100%",
-      borderRadius: "inherit",
     },
     "& .media-overlay": {
       position: "absolute",
       width: "100%",
       height: "100%",
       backgroundColor: "rgba(7, 27, 33, 0.3)",
-      borderRadius: "inherit",
     },
   },
   "&:hover": {
@@ -103,11 +49,7 @@ const SimpleCard = ({
   return (
     <Root container sx={sx} {...props} direction="column" spacing={2}>
       <Grid item xs={imgRatio} className="media-container">
-        {overlay === true ? (
-          <CardMedia image="" title="" className="media-overlay" />
-        ) : (
-          <></>
-        )}
+        {overlay === true ? <Box className="media-overlay"></Box> : <></>}
         <CardMedia image={img} title="" className="media-image" />
       </Grid>
       <Grid item xs={contentRatio}>
@@ -118,7 +60,6 @@ const SimpleCard = ({
       </Grid>
     </Root>
   );
->>>>>>> 2b0105034f352842fbcaf2ed4a6098cee735a1f4
 };
 
 export default SimpleCard;

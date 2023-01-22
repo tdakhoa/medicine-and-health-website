d0c1e9949fa09b8be14be35396c19c82c2f8ca70
nganlamforwork <nganlamforwork@gmail.com>
2023-01-22 20:16:16 +0700

    update filename

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Heart from "./Sections/Heart";
 import Hero from "./Sections/Hero";
-import Medical from "./Sections/Medical";
+import NormalSection from "./Sections/Normal";
 import Meds from "./Sections/Meds";
 import News from "./Sections/News";
 import Diseases from "./Sections/Diseases";
@@ -11,12 +11,12 @@ const Home = () => {
   return (
     <>
       <NavBar />
+      <News />
+      <NormalSection />
       <Hero />
       <Diseases />
       <Heart />
-      <Medical />
       <Meds />
-      <News />
     </>
   );
 };
---
+++ src/modules/Home/Sections/Normal.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NormalSection = () => {
+  return <div>Medical</div>;
+};
+
+export default NormalSection;

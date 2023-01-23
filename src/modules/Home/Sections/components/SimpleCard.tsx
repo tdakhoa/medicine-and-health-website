bb0c86ee85f24ec3cda2330ce9a7a67da9caaab1
nganlamforwork <nganlamforwork@gmail.com>
2023-01-23 14:09:16 +0700

    update simple card

--- src/modules/Home/Sections/components/SimpleCard.tsx
+++ src/modules/Home/Sections/components/SimpleCard.tsx
@@ -2,12 +2,14 @@ import { styled, Box, CardMedia, Grid } from "@mui/material";
 import React from "react";
 import { Typography } from "../../../../components";
 
-const Root = styled(Grid)(({ theme }) => ({
+const Root = styled(Grid)((props: SimpleCardProps) => ({
   cursor: "pointer",
   transition: "all .1s ease-in-out",
   "& .media-container": {
     position: "relative",
     overflow: "hidden",
+    width: "100%",
+    height: "100%",
     "& .media-image": {
       width: "100%",
       height: "100%",
@@ -34,6 +36,7 @@ interface SimpleCardProps {
   imgRatio?: number;
   contentRatio?: number;
   overlay?: boolean;
+  borderRadius?: string;
   sx?: object;
 }
 const SimpleCard = ({
@@ -43,14 +46,20 @@ const SimpleCard = ({
   imgRatio = 6,
   contentRatio = 6,
   overlay = true,
+  borderRadius = "0px",
   sx = {},
   ...props
 }: SimpleCardProps) => {
   return (
     <Root container sx={sx} {...props} direction="column" spacing={2}>
-      <Grid item xs={imgRatio} className="media-container">
-        {overlay === true ? <Box className="media-overlay"></Box> : <></>}
-        <CardMedia image={img} title="" className="media-image" />
+      <Grid item xs={imgRatio}>
+        <Box
+          className="media-container"
+          sx={{ borderRadius: `${borderRadius}` }}
+        >
+          {overlay === true ? <Box className="media-overlay"></Box> : <></>}
+          <CardMedia image={img} title="" className="media-image" />
+        </Box>
       </Grid>
       <Grid item xs={contentRatio}>
         <Typography size="h6" weight="bold">

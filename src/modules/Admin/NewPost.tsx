ef738ef827b850f33dfc8e5d1f0864cc6c80bc07
nganlamforwork <nganlamforwork@gmail.com>
2023-01-25 01:29:10 +0700

    almost done except add img

--- src/modules/Admin/NewPost.tsx
+++ src/modules/Admin/NewPost.tsx
@@ -1,10 +1,13 @@
 import { styled, Box, Grid } from "@mui/material";
 import React from "react";
-import { Dropdown, TextField, Typography } from "../../components";
+import { Button, Dropdown, TextField, Typography } from "../../components";
 import ToggleDrawer from "./components/Drawer";
+import TextEditor from "../../components/Input/TextEditor";
+import { AccessTimeOutlined, CancelOutlined, SaveOutlined } from "@mui/icons-material";
 
 const InputContainer = styled(Grid)(() => ({
   width: "100%",
+  margin: 0,
 }));
 const RowItem = { flexGrow: 1 };
 
@@ -32,9 +35,43 @@ const NewPost = () => {
               />
             </Grid>
             <Grid item xs={12}>
+              <TextEditor label="Nội dung bài viết" />
+            </Grid>
+            <Grid item xs={12}>
               <TextField label="Tác giả" placeholder="Nhập tác giả bài viết" />
             </Grid>
           </InputContainer>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row-reverse",
+              justifyContent: "end",
+              gap: "2rem",
+              mt: "3rem",
+            }}
+          >
+            <Button
+              bgColor="primary"
+              borderRadius="10px"
+              endIcon={<SaveOutlined sx={{ fontSize: "1.4rem", pl: "0.3rem" }} />}
+            >
+              <Typography size="p">Lưu và đăng ngay</Typography>
+            </Button>
+            <Button
+              bgColor="secondary"
+              borderRadius="10px"
+              endIcon={<AccessTimeOutlined sx={{ fontSize: "1.4rem", pl: "0.3rem" }} />}
+            >
+              <Typography size="p">Lưu thành bản nháp</Typography>
+            </Button>
+            <Button
+              bgColor="gray"
+              borderRadius="10px"
+              endIcon={<CancelOutlined sx={{ fontSize: "1.4rem", pl: "0.3rem" }} />}
+            >
+              <Typography size="p">Huỷ và không lưu</Typography>
+            </Button>
+          </Box>
         </Box>
       </Box>
     </>

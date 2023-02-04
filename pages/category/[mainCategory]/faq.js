956a21711cb32dd563b62bda1cbd16740f83e6eb
Trần Đỗ Anh Khoa <anhkhoatrando123@gmail.com>
2023-02-04 22:29:56 +0700

    add FAQ

---
+++ pages/category/[mainCategory]/faq.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Head from "next/head";
+import FAQ from "../../../src/modules/Category/FAQ";
+
+export default function HomePage() {
+    return (
+        <div>
+            <Head>
+                <title>THUỐC & SỨC KHỎE</title>
+                <link rel="icon" href="/Logo.png" />
+            </Head>
+
+            <main>
+                <FAQ />
+            </main>
+        </div>
+    );
+}

ebe087500156a8593ac65c0258fc1935b12afbec
Trần Đỗ Anh Khoa <anhkhoatrando123@gmail.com>
2023-02-10 17:43:34 +0700

    mobile

--- src/modules/Home/Sections/Mix.tsx
+++ src/modules/Home/Sections/Mix.tsx
@@ -21,27 +21,20 @@ const Mix = () => {
                     title={cardData[0].title}
                     content={cardData[0].content}
                 />
-
-                {cardData.map((data, id) =>
-                    id !== 0 ? (
-                        <AlternateCard
-                            key={id}
-                            direction={id % 2 == 1 ? "row" : "row-reverse"}
-                            sx={{ display: { xs: "flex", md: "none" } }}
-                            img={data.img}
-                            title={data.title}
-                            content={data.content}
-                        />
-                    ) : (
-                        <div key={id}></div>
-                    )
-                )}
             </CardContainer>
 
-            <Grid sx={{ display: { xs: "none", md: "flex" } }} container rowSpacing={0} columnSpacing={2}>
+            <Grid
+                sx={{
+                    "& div:nth-of-type(3), div:nth-of-type(4)": {
+                        display: "none"
+                    }
+                }}
+                container
+                rowSpacing={0}
+                columnSpacing={2}>
                 {cardData.map((data, i) =>
                     i !== 0 ? (
-                        <Grid item xs={3} key={i}>
+                        <Grid item xs={6} md={3} key={i}>
                             <SimpleCard
                                 img={data.img}
                                 title={data.title}
